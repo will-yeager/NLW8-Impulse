a539dd2993827b07d0e049b23a69ca581fe5d14c
William <willyeager01@gmail.com>
2022-05-10 19:02:42 -0300

    finished all classes and preparing for deploy

---
+++ mobile/src/components/Form/index.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { ArrowLeft } from 'phosphor-react-native';
+import { captureScreen } from 'react-native-view-shot'
+import { View, TextInput, Image, Text, TouchableOpacity  } from 'react-native';
+import { FeedbackType } from '../Widget';
+import { ScreenshotButton } from '../ScreenshotButton';
+import { Button } from '../Button';
+
+import * as FileSystem from 'expo-file-system';
+import { theme } from '../../theme';
+import { styles } from './styles';
+import { feedbackTypes } from '../../utils/feedbackTypes'
+import { api } from '../../libs/api';
+
+
+interface Props {
+    feedbackType: FeedbackType;
+    onFeedbackCanceled: () => void;
+    onFeedbackSent: () => void;
+}
+
+export function Form({feedbackType, onFeedbackCanceled, onFeedbackSent}: Props) {
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+    const [screenshot, setScreenshot] = useState<string | null>(null)
+    const [comment, setComment] = useState("")
+
+    const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+    function handleScreenshot() {
+        captureScreen({
+            format: 'jpg',
+            quality: 0.8
+        })
+            .then(uri => setScreenshot(uri))
+            .catch(error => console.log(error))
+    }
+
+    function handleScreenshotRemove() {
+        setScreenshot(null);
+    }
+    
+    async function handleSendFeedback() {
+        if (isSendingFeedback) {
+            return
+        }
+        setIsSendingFeedback(true);
+        const screenshotBase64 = screenshot && await FileSystem.readAsStringAsync(screenshot, { encoding: 'base64' });
+        const screenshotForSend = `data:image/png;base64, ${screenshotBase64}`
+
+        try {
+            await api.post('/feedbacks', {
+                type: feedbackType,
+                screenshot: screenshotForSend,
+                comment
+            })
+            onFeedbackSent();
+        } catch (error) {
+            console.log(error);
+            setIsSendingFeedback(false);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={onFeedbackCanceled}>
+
+                <ArrowLeft
+                    size={24}
+                    weight="bold"
+                    color={theme.colors.text_secondary}
+                />
+                </TouchableOpacity>
+            
+                <View style={styles.titleContainer}>
+                    <Image
+                        source={feedbackTypeInfo.image}
+                        style={styles.image}
+                    />
+                    <Text style={styles.titleText}>
+                        {feedbackTypeInfo.title}
+                    </Text>
+                </View>
+            </View>
+
+            <TextInput
+                multiline
+                style={styles.input}
+                placeholder="Algo não está funcinando bem? Queremos corrigir. Conte com deatlhes o que está acontecendo...."
+                placeholderTextColor={theme.colors.text_secondary}
+                autoCorrect={false}
+                onChangeText={setComment}
+            />
+
+
+
+            <View style={styles.footer}>
+                <ScreenshotButton
+                    onTakeShot={handleScreenshot}
+                    onRemoveShot={handleScreenshotRemove}
+                    screenshot={screenshot}
+                />
+                <Button
+                    onPress={handleSendFeedback}
+                    isLoading={isSendingFeedback}
+                />
+            </View>
+        </View>
+    )
+}
